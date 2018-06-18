83769c00b2ae7ac2745fac6fce45a6030727592d
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-06-18 16:45:12 -0700

    Added Cards.

---
+++ src/scripts/components/Card.tsx
@@ -0,0 +1,51 @@
+﻿import * as React from 'react';
+
+import { GridSize, gridConfig } from './Grid';
+
+export interface ICardProps {
+    id?: string;
+    className?: string;
+    closed?: boolean;
+    header?: any;
+    footer?: any;
+    nav?: any;
+    navAlign?: 'start' | 'end';
+    grid?: boolean;
+    gridColumns?: number;
+    gridSize?: GridSize;
+}
+
+export default class Card extends React.Component<ICardProps, any> {
+    render() {
+        let {
+            id,
+            className,
+            closed,
+            header,
+            footer,
+            nav,
+            navAlign,
+            grid,
+            gridColumns,
+            gridSize,
+        } = this.props;
+        let classNames = className ? [className] : [];
+        classNames.push('card');
+
+        let innerClassNames = ['card-content'];
+        if (grid) {
+            gridConfig(innerClassNames, gridColumns, gridSize);
+        }
+
+        return (
+            <div className={classNames.join(' ')} id={id} data-closed={closed}>
+                {header ? <header>{header}</header> : null}
+                <div className={innerClassNames.join(' ')}>
+                    {this.props.children}
+                </div>
+                {nav ? <nav className={navAlign === 'end' ? 'card-nav-align-end' : ''}>{nav}</nav> : null}
+                {footer ? <footer>{footer}</footer> : null}
+            </div>
+        );
+    }
+}
