fe6fb040482774bfcbd139f66de761537c30adec
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-06-27 17:52:29 -0700

    Changed BodyScroll to be more configurable.

--- src/scripts/util/BodyScroll.ts
+++ src/scripts/util/BodyScroll.ts
@@ -1,15 +1,17 @@
 export default class BodyScroll {
     static lockCount: number = 0;
+    static rootSelector: string = 'body > .root';
+    static bodyLockClass: string = 'body-scroll-lock';
 
     static lock() {
         this.lockCount++;
 
         let body = document.body;
-        let root = document.querySelector('body > .root');
+        let root = document.querySelector(this.rootSelector);
         if (root) {
             // We must query multiple objects for the scrollTop.
             let scrollTop = window.pageYOffset || document.documentElement.scrollTop || body.scrollTop || root.scrollTop;
-            body.classList.add('body-scroll-lock');
+            body.classList.add(this.bodyLockClass);
             root.scrollTop = scrollTop;
         }
     }
@@ -21,10 +23,10 @@ export default class BodyScroll {
         }
 
         let body = document.body;
-        let root = document.querySelector('body > .root');
+        let root = document.querySelector(this.rootSelector);
         if (root && this.lockCount === 0) {
             let scrollTop = root.scrollTop || window.pageYOffset || document.documentElement.scrollTop || body.scrollTop;
-            body.classList.remove('body-scroll-lock');
+            body.classList.remove(this.bodyLockClass);
             // We must set both of these for Chrome and Firefox.
             body.scrollTop = scrollTop;
             document.documentElement.scrollTop = scrollTop;
