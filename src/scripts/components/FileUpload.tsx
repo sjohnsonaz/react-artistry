1c33c5dde9f66c053c86f359702dee5fa79695c4
Sean Johnson <sjohnson@sjohnsonaz.com>
2018-09-21 15:19:28 -0700

    Fixed FileUpload onClose return type.

--- src/scripts/components/FileUpload.tsx
+++ src/scripts/components/FileUpload.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 export interface IFileUploadProps {
     id?: string;
     className?: string;
-    onUpload: (files: FileList) => Promise<string[]>;
+    onUpload: (files: FileList) => any;
     text?: string;
 }
 
