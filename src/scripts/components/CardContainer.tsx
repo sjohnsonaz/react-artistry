a8664273f2c1c16128fdc80302912cb763046191
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-01-22 16:10:04 -0700

    Added space prop to CardContainer.

--- src/scripts/components/CardContainer.tsx
+++ src/scripts/components/CardContainer.tsx
@@ -3,17 +3,23 @@ import * as React from 'react';
 export interface ICardContainerProps {
     id?: string;
     className?: string;
+    space?: boolean;
 }
 
 export default class CardContainer extends React.Component<ICardContainerProps, any> {
     render() {
         let {
             id,
-            className
+            className,
+            space
         } = this.props;
 
         let classNames = className ? [className] : [];
         classNames.push('card-container');
+
+        if (space) {
+            classNames.push('space');
+        }
 
         return (
             <div className={classNames.join(' ')} id={id}>
