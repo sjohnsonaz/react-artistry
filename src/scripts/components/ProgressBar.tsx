c1d888c5c24526f47998f7404f8a3659d82f5d9d
Sean Johnson <sjohnson@sjohnsonaz.com>
2019-02-28 00:18:53 -0700

    Updated ProgressBar to use progress-bar-content.

--- src/scripts/components/ProgressBar.tsx
+++ src/scripts/components/ProgressBar.tsx
@@ -11,6 +11,7 @@ export default class ProgressBar extends React.Component<IProgressBarProps, any>
     render() {
         let classNames = this.props.className ? [this.props.className] : [];
         classNames.push('progress-bar');
+        classNames.push('progress-bar-content')
 
         let value = this.props.value;
         if (value < 0) {
@@ -19,13 +20,13 @@ export default class ProgressBar extends React.Component<IProgressBarProps, any>
         if (value > 100) {
             value = 100;
         }
-        let style = { width: value + '%' };
+        let style = { "--progress-bar-progress": value + '%' };
 
         let title = this.props.title || value + '%'
 
         return (
             <div className={classNames.join(' ')} id={this.props.id}>
-                <div style={style}>
+                <div style={style as any}>
                     {title}
                 </div>
             </div>
