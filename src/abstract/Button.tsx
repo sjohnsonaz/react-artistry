b303eec8c7fa2ce0905ce1876fadd7b38c1043c9
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-06-22 04:34:43 -0700

    Added Wrap mixin to Button.

--- src/abstract/Button.tsx
+++ src/abstract/Button.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { addContext, block, Settings } from '@artistry/abstract';
-import { Paper, IDefaultSettings } from 'artistry';
+import { addContext, block, Settings, NOWRAP } from '@artistry/abstract';
+import { Paper, IDefaultSettings, Wrap } from 'artistry';
 
 const classes = addContext(() => {
     let base = Settings.get<IDefaultSettings>();
     const button = block('button',
         Paper({
             color: base.colors.primary
+        }),
+        Wrap({
+            whiteSpace: NOWRAP
         })
     );
 
