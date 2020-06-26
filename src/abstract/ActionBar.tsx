015187c5661f8d455b18f06915338b2ef9911b50
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-06-26 01:12:14 -0700

    Changed ActionBar to use Box, Rule, and Attr. Added data function.

--- src/abstract/ActionBar.tsx
+++ src/abstract/ActionBar.tsx
@@ -1,45 +1,80 @@
 import React from 'react';
-import { block, addContext } from '@artistry/abstract';
-import { Paper, getSettings, FlexContainer, Flex } from 'artistry';
+import { block, addContext, FLEX_START, FLEX_END, CENTER, SPACE_BETWEEN, SPACE_AROUND, SPACE_EVENTLY } from '@artistry/abstract';
+import { Attr, Box, FlexContainer, Rule } from 'artistry';
+import { data } from '../util';
 
 let classes = addContext(() => {
-    const base = getSettings();
+    const DIRECTION = 'direction';
+    const JUSTIFY = 'justify';
     const ActionBar = block('action-bar',
-        Paper({
+        Box({
             padding: 0,
-            margin: base.space(1, 0.5)
+            margin: [1, 0.5]
         }),
         FlexContainer({
             justify: 'flex-end'
+        }),
+        Rule('& > *',
+            Box({
+                margin: [0, 0.5]
+            })
+        ),
+        Attr(DIRECTION, {
+            forward: FlexContainer({
+                direction: 'row'
+            }),
+            reverse: FlexContainer({
+                direction: 'row-reverse'
+            })
+        }),
+        Attr(JUSTIFY, {
+            start: FlexContainer({
+                justify: FLEX_START
+            }),
+            end: FlexContainer({
+                justify: FLEX_END
+            }),
+            center: FlexContainer({
+                justify: CENTER
+            }),
+            'space-between': FlexContainer({
+                justify: SPACE_BETWEEN
+            }),
+            'space-around': FlexContainer({
+                justify: SPACE_AROUND
+            }),
+            'space-evenly': FlexContainer({
+                justify: SPACE_EVENTLY
+            }),
         })
     );
-    ActionBar.rule('& > *', {
-        margin: base.space(0, 0.5)
-    });
-    ActionBar.attr('direction', {
-        forward: Flex({
-            direction: 'row'
-        }),
-        reverse: Flex({
-            direction: 'row-reverse'
-        })
-    });
-
     return {
-        ActionBar
+        ActionBar,
+        DIRECTION,
+        JUSTIFY
     };
 });
 
 export interface IActionBarProps {
     children?: any;
     direction?: 'forward' | 'reverse';
+    justify?: 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
 }
 
-export const ActionBar: React.FC<IActionBarProps> = ({ children, direction }) => {
+export const ActionBar: React.FC<IActionBarProps> = ({
+    children,
+    direction,
+    justify
+}) => {
+
     return (
         <div
             className={classes.ActionBar}
-            data-direction={direction}
-        >{children}</div>
+            {...{
+                [data(classes.DIRECTION)]: direction,
+                [data(classes.JUSTIFY)]: justify,
+            }}>
+            {children}
+        </div>
     );
 };
