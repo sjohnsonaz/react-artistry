bf1239ae129dd07a37a3b46a2bdf32c59a50c296
Sean Johnson <sjohnson@sjohnsonaz.com>
2020-06-26 04:00:32 -0700

    Added constants to ActionBar.

--- src/abstract/ActionBar.tsx
+++ src/abstract/ActionBar.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { block, addContext, FLEX_START, FLEX_END, CENTER, SPACE_BETWEEN, SPACE_AROUND, SPACE_EVENTLY } from '@artistry/abstract';
+import { block, addContext, FLEX_START, FLEX_END, CENTER, SPACE_BETWEEN, SPACE_AROUND, SPACE_EVENLY } from '@artistry/abstract';
 import { Attr, Box, FlexContainer, Rule } from 'artistry';
 import { data } from '../util';
 
 const DIRECTION = 'direction';
+const DIRECTION_FORWARD = 'forward';
+const DIRECTION_REVERSE = 'reverse';
+
 const JUSTIFY = 'justify';
+const JUSTIFY_START = 'start';
+const JUSTIFY_END = 'end';
+const JUSTIFY_CENTER = 'center';
+const JUSTIFY_SPACE_BETWEEN = 'space-between';
+const JUSTIFY_SPACE_AROUND = 'space-around';
+const JUSTIFY_SPACE_EVENLY = 'space-evenly';
+
 let classes = addContext(() => {
     const ActionBar = block('action-bar',
         Box({
@@ -20,31 +30,31 @@ let classes = addContext(() => {
             })
         ),
         Attr(DIRECTION, {
-            forward: FlexContainer({
+            [DIRECTION_FORWARD]: FlexContainer({
                 direction: 'row'
             }),
-            reverse: FlexContainer({
+            [DIRECTION_REVERSE]: FlexContainer({
                 direction: 'row-reverse'
             })
         }),
         Attr(JUSTIFY, {
-            start: FlexContainer({
+            [JUSTIFY_START]: FlexContainer({
                 justify: FLEX_START
             }),
-            end: FlexContainer({
+            [JUSTIFY_END]: FlexContainer({
                 justify: FLEX_END
             }),
-            center: FlexContainer({
+            [JUSTIFY_CENTER]: FlexContainer({
                 justify: CENTER
             }),
-            'space-between': FlexContainer({
+            [JUSTIFY_SPACE_BETWEEN]: FlexContainer({
                 justify: SPACE_BETWEEN
             }),
-            'space-around': FlexContainer({
+            [JUSTIFY_SPACE_AROUND]: FlexContainer({
                 justify: SPACE_AROUND
             }),
-            'space-evenly': FlexContainer({
-                justify: SPACE_EVENTLY
+            [JUSTIFY_SPACE_EVENLY]: FlexContainer({
+                justify: SPACE_EVENLY
             }),
         })
     );
@@ -53,10 +63,21 @@ let classes = addContext(() => {
     };
 });
 
+type Direction =
+    typeof DIRECTION_FORWARD |
+    typeof DIRECTION_REVERSE;
+type Justify =
+    typeof JUSTIFY_START |
+    typeof JUSTIFY_END |
+    typeof JUSTIFY_CENTER |
+    typeof JUSTIFY_SPACE_BETWEEN |
+    typeof JUSTIFY_SPACE_AROUND |
+    typeof JUSTIFY_SPACE_EVENLY;
+
 export interface IActionBarProps {
     children?: any;
-    direction?: 'forward' | 'reverse';
-    justify?: 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+    direction?: Direction;
+    justify?: Justify;
 }
 
 export const ActionBar: React.FC<IActionBarProps> = ({
